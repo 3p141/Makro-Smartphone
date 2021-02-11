b48357b04c255fd4e50e616ce58b035f8ecdb76e
3p41 <matti.lubahn@outlook.com>
2021-02-11 13:44:48 +0100

    added Buttons on Website

---
+++ src/controller/WebsiteController.java
@@ -0,0 +1,18 @@
+package kickstart.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WebsiteController {
+
+	@GetMapping("/")
+	public String index() {
+		return "start";
+	}
+
+	@GetMapping("/client")
+	public String showClock(){
+		return "client";
+	}
+}
